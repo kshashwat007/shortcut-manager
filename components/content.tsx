611f9eb4b3cc769ef45a1bcc72c2587be4b6b0fe
kshashwat007 <kshashwt007@gmail.com>
2023-10-29 13:39:40 +0530

    data object

--- components/content.tsx
+++ components/content.tsx
@@ -3,23 +3,14 @@ import { GrAddCircle } from "react-icons/gr";
 import { FaReact } from 'react-icons/fa';
 import { FaCirclePlus } from 'react-icons/fa6';
 
-const dataExcel = [
-  {
-      shortcut_name: "Select All", shortcut_key: "Ctrl + C",
-  },
-  {
-      shortcut_name: "Applies Bold formatting", shortcut_key: "Ctrl + B"
-  }
-]
+const data = {
+  "excel": [{ shortcut_name: "Select All", shortcut_key: "Ctrl + C" }, { shortcut_name: "Applies Bold formatting", shortcut_key: "Ctrl + B" }],
+  "vscode": [{ shortcut_name: "Select All", shortcut_key: "Ctrl + C" }, { shortcut_name: "Applies Bold formatting", shortcut_key: "Ctrl + B" }]
+}
 
-const dataVscode = [
-  {
-      shortcut_name: "Select All", shortcut_key: "Ctrl + C",
-  },
-  {
-      shortcut_name: "Applies Bold Formatting", shortcut_key: "Ctrl + B"
-  }
-]
+function capitalizeFirstLetter(str: string): string {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
 const Content = () => {
   return (
@@ -30,34 +21,23 @@ const Content = () => {
           <a className="link">Create Shortcut</a>
         </div>
         
-        <div className="collapse collapse-arrow join-item border border-base-300">
-            <input type="radio" name="my-accordion-4" /> 
-            <div className="collapse-title text-md font-medium">
-            Excel
-            </div>
-            <div className="collapse-content"> 
-              {dataExcel.map(({shortcut_name,shortcut_key}) => (
-                  <div key={shortcut_name} className='flex flex-row justify-between hover:bg-[#141414] p-2'>
-                      <p className='font-bold text-l'>{shortcut_name}</p>
-                      <kbd className="kbd">{shortcut_key}</kbd>
-                  </div>
-              ))}
-            </div>
-        </div>
-        <div className="collapse collapse-arrow join-item border border-base-300">
-            <input type="radio" name="my-accordion-4" /> 
-            <div className="collapse-title text-md font-medium">
-            Vscode
-            </div>
-            <div className="collapse-content"> 
-              {dataVscode.map(({shortcut_name,shortcut_key}) => (
-                  <div key={shortcut_name} className='flex flex-row justify-between hover:bg-[#141414] p-2'>
-                      <p className='font-bold text-l'>{shortcut_name} </p>
-                      <kbd className="kbd">{shortcut_key}</kbd>
-                  </div>
-              ))}
-            </div>
-        </div>
+      {Object.keys(data).map((appName) => (
+        <div className="collapse collapse-arrow join-item border border-base-300" key={appName}>
+          <input type="radio" name="my-accordion-4" /> 
+          <div className="collapse-title text-md font-medium">
+          {capitalizeFirstLetter(appName)}
+          </div>
+          <div className="collapse-content"> 
+            {data[appName].map(({shortcut_name,shortcut_key}) => (
+                <div key={shortcut_name} className='flex flex-row justify-between hover:bg-[#141414] p-2'>
+                    <p className='font-bold text-l'>{shortcut_name}</p>
+                    <kbd className="kbd">{shortcut_key}</kbd>
+                </div>
+            ))}
+          </div>
+      </div>
+      ))}
+        
     </div>
   )
 }

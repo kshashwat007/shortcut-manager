c10888c84f14f30385572446b173f95cbcbf4f91
kshashwat007 <kshashwt007@gmail.com>
2023-11-03 21:55:08 +0530

    search and shortcut changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,45 @@
 'use client'
 
 import Image from 'next/image'
-import { register } from '@tauri-apps/api/globalShortcut'
+import { register,unregister } from '@tauri-apps/api/globalShortcut'
 import { invoke } from '@tauri-apps/api/tauri';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Favourites from '@/components/favourites';
 import Content from '@/components/content';
-
-
 
 function toggleVisibility() {
   invoke('toggle_visibility')
       .catch((error) => {
           console.error(error);
       });
-  
+}
+
+async function registerTauri() {
+  await register('CommandOrControl+Shift+C', () => {
+    console.log('Shortcut triggered');
+    toggleVisibility();
+    console.log("Window Open")
+  });
+}
+
+async function unregisterTauri() {
+  await unregister('CommandOrControl+Shift+C');
 }
 
 export default function Home() {
+  const [searchQuery, setSearchQuery] = useState("")
+  
   useEffect(() => {
-    const registerTauri = async () => {
-      await register('CommandOrControl+Shift+C', () => {
-        console.log('Shortcut triggered');
-        toggleVisibility();
-        console.log("Window Open")
-      });
-    }
     registerTauri();
 
     return () => {
-      
+      unregisterTauri()
     }
   }, [])
   return (
     <main className="flex flex-col items-center justify-between ">
-      <input type="text" placeholder="Type here" className="input input-bordered w-full" />
-      <Content />
+      <input type="text" placeholder="Type here" className="input input-bordered w-full" onChange={(e) => setSearchQuery(e.target.value)}/>
+      <Content searchQuery={searchQuery}/>
     </main>
   )
 }
--- components/content.tsx
+++ components/content.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GrAddCircle } from "react-icons/gr";
 import { FaReact } from 'react-icons/fa';
 import { FaCirclePlus } from 'react-icons/fa6';
@@ -21,30 +21,32 @@ function capitalizeFirstLetter(str: string): string {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-const Content = () => {
+const Content = ({searchQuery}: {searchQuery: string}) => {
+  
+  const filteredAppNames = Object.keys(data).filter((appName) =>
+    appName.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div className="join join-vertical w-full">
-
-        <div className='p-2 m-2 flex flex-row gap-3 items-center'>
-          <FaCirclePlus style={{ color: 'white', fontSize: '1 rem' }} />
-          <a className="link">Create Shortcut</a>
-        </div>
-        
-      {Object.keys(data).map((appName) => (
+      <div className='p-2 m-2 flex flex-row gap-3 items-center'>
+        <FaCirclePlus style={{ color: 'white', fontSize: '1 rem' }} />
+        <a className="link">Create Shortcut</a>
+      </div>
+      
+      {filteredAppNames.map((appName) => (
         <div className="collapse collapse-arrow join-item border border-base-300" key={appName}>
-          <input type="radio" name="my-accordion-4" /> 
-          <div className="collapse-title text-md font-medium">
-          {capitalizeFirstLetter(appName)}
-          </div>
-          <div className="collapse-content"> 
-            {data[appName].map(({shortcut_name,shortcut_key}) => (
-                <div key={shortcut_name} className='flex flex-row justify-between hover:bg-[#141414] p-2'>
-                    <p className='font-bold text-l'>{shortcut_name}</p>
-                    <kbd className="kbd">{shortcut_key}</kbd>
-                </div>
+          <input type="radio" name="my-accordion-4" />
+          <div className="collapse-title text-md font-medium">{capitalizeFirstLetter(appName)}</div>
+          <div className="collapse-content">
+            {data[appName].map(({ shortcut_name, shortcut_key }) => (
+              <div key={shortcut_name} className="flex flex-row justify-between hover:bg-[#141414] p-2">
+                <p className="font-bold text-l">{shortcut_name}</p>
+                <kbd className="kbd">{shortcut_key}</kbd>
+              </div>
             ))}
           </div>
-      </div>
+        </div>
       ))}
         
     </div>
